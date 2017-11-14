261e2a36c22c37253f20da97d1cde7ce1ecfcdab
Sergmilioner <milioner0803>
2017-11-14 18:44:49 +0200

    My  firat commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<select>
+
+    <?php
+    for ($i = 1910; $i<=2010;$i++){
+        echo "<option value=\"$i\">$i</option>";
+    }
+    ?>
+</select>
+
+
+
+
+<?php
+     $i = 1;
+     do
+     {echo $i;
+     $i++;
+     }
+      while($i<10);
+?>
+
