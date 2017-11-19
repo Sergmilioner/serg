25045a7ac29802f89b60249c0ed4eea56bdc6f56
Sergmilioner <milioner0803>
2017-11-19 14:04:15 +0200

    sunday 19/11/2017 14.04

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
-//$array = [4,5,6,72,432,565];
-//asort($array);
-//var_dump($array);
-////foreach($array as $key =>$val) {
-////    echo "$key = $val"."</br>";
-////}
-//
+$array = [4,5,6,72,432,565];
+asort($array);
+var_dump($array);
+//foreach($array as $key =>$val) {
+//    echo "$key = $val"."</br>";
+//}
+
 
 //
 //$file = 'people.txt';
