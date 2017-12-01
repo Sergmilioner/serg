b14f7155386ba4598e70150dadcd9d9c366fddfe
Sergmilioner <milioner0803>
2017-12-01 17:35:40 +0200

    forma obratnoj svazi

--- example.php
+++ example.php
@@ -1,20 +1,44 @@
-
 <?php
-$q = array(2, 5, -9, 7, -6, 1, 2, -45, 463, -26, 18, 16, -16,-100,-200);
-$fl=true;
-while ($fl)
-{
-    $fl=false;
-    for ($i = 0; $i < count($q) - 1; $i++) {
-        if ($q[$i] > $q[$i + 1]) {
-            $vr = $q[$i];
-            $q[$i] = $q[$i + 1];
-            $q[$i + 1] = $vr;
-            $fl=true;
-
-        }
-    }
+if (isset($_POST['send'])){
+    echo '<pre>';
+    print_r($_POST);
+    echo '</pre>';
 }
-var_dump($q);
-?>
+    if(!empty($_FILES))
+    {
+        echo '<pre>';
+        print_r($_FILES);
+        echo '<pre>';
+        move_uploaded_file($_FILES['file']['tmp_name'],'upload/'.$_FILES['file']['name']);?>
+        <img src="upload/<?=$_FILES['file']['tmp_name']?>"<?php
+    }
 
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+     <form method="post" action=""enctype="multipart/form-data">
+         <p>
+             Neme:<input type="text" name="name"value="<?=($_POST['name']) ? $_POST['name']: ""?>""
+         </p>
+         <p>
+             Text:<textarea name="text"></textarea>
+         </p>
+         <p>
+             <input type="file"name="file">
+         </p>
+         <p>
+             <button type="submit" name="send" value="test">Send</button>
+         </p>
+     </form>
+ <img src="upload/5555555555555555555555555555.png">
+</body>
+</html>
--- fileName.php
+++ fileName.php
@@ -1,10 +1,42 @@
-<?php
-$array = [4,5,6,72,432,565];
-asort($array);
-var_dump($array);
-//foreach($array as $key =>$val) {
-//    echo "$key = $val"."</br>";
-//}
 
-drer
+
+
+<?php
+     if(isset($_POST["send"])) //проверка нажата ли кнопка отправить
+     {
+         $from= htmlspecialchars($_POST["from"]);// обработаем html код , который не будет обрабатыватся
+         $to= htmlspecialchars($_POST["to"]);
+         $subject= htmlspecialchars($_POST["file"]);
+         $_SESSION["from"]=$from;
+         $error_from = "";
+         if($from ==""||preg_match("/@/",$from)){
+             $error_from ="Ваша заявка не принята";
+             $error = true;
+         }
+     }
 ?>
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+
+    <title>Обратная связь</title>
+</head>
+<body>
+<h2>Форма обратной связи</h2>
+<form name="feedback" action=""method="post">
+    <p>
+        Email:<input type="text" name="from" value="<?php echo $_SESSION["from"]?>"/>
+        <span style="color:red"><?=$error_from?></span>
+    </p>
+    <p>
+        Text:<input type="text"  name="to"/>
+
+    </p>
+        File:<input type="file"name="file">
+    <p>
+        <button type="submit" name="send" value="отправить">Отправить</button>
+    </p>
+</form>
+</body>
+</html>
